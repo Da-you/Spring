e7e02dd5c62b495d04ba3a5f7102ffa1177717f7
JoHyunwoo <jhw3874@gmail.com>
2023-08-18 00:25:14 +0900

    타임 리프 추가중

--- 게시판서비스/demo/src/main/java/com/example/demo/controller/ArticleController.java
+++ 게시판서비스/demo/src/main/java/com/example/demo/controller/ArticleController.java
@@ -1,9 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,6 +14,12 @@ public class ArticleController {
     public String articles(ModelMap map){
         map.addAttribute("articles", List.of());
         return "articles/index";
+    }
+    @GetMapping("/{articleId}")
+    public String articles(@PathVariable Long articleId, ModelMap map){
+        map.addAttribute("article", null);
+        map.addAttribute("articleComment", List.of());
+        return "articles/detail";
     }
 
 }
