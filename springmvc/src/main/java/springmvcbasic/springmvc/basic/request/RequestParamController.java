107a90f852c381bd7f49e020e263402bc96b7876
hyunwoo <dayou346@gmail.com>
2023-09-11 17:06:56 +0900

    HTTP 요청 파라미터 - 쿼리 파라미터, HTML Form 2

--- springmvc/src/main/java/springmvcbasic/springmvc/basic/request/RequestParamController.java
+++ springmvc/src/main/java/springmvcbasic/springmvc/basic/request/RequestParamController.java
@@ -4,7 +4,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
 
@@ -18,5 +21,29 @@ public class RequestParamController {
         int age = Integer.parseInt(request.getParameter("age"));
         log.info("username={}, age={}", username, age);
         response.getWriter().write("ok");
+    }
+
+    @ResponseBody // view 조회를 무시하고 HTTP message body에 직접 해당 내용 입력
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(
+            @RequestParam("username") String memberName,
+            @RequestParam("age") int memberAge) {
+        log.info("username={}, age={}", memberName, memberAge);
+        return "ok";
+    }
+    @ResponseBody // view 조회를 무시하고 HTTP message body에 직접 해당 내용 입력
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String username,
+            @RequestParam int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+    @ResponseBody // view 조회를 무시하고 HTTP message body에 직접 해당 내용 입력
+    @RequestMapping("/request-param-v4")
+    // String, int, Integer 등의 단순 타입이면 @RequestParam 도 생략 가능
+    public String requestParamV4(String username, int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
     }
 }
