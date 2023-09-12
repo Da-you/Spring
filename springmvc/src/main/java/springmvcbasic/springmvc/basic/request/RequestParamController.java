230f573493756b95d94e47fc8b255da0020046ab
JoHyunwoo <jhw3874@gmail.com>
2023-09-12 14:37:00 +0900

    HTML 요청 파라미터 - @RequsestParam

--- springmvc/src/main/java/springmvcbasic/springmvc/basic/request/RequestParamController.java
+++ springmvc/src/main/java/springmvcbasic/springmvc/basic/request/RequestParamController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -44,6 +45,28 @@ public class RequestParamController {
     // String, int, Integer 등의 단순 타입이면 @RequestParam 도 생략 가능
     public String requestParamV4(String username, int age) {
         log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+    @ResponseBody // view 조회를 무시하고 HTTP message body에 직접 해당 내용 입력
+    @RequestMapping("/request-param-required")
+    // String, int, Integer 등의 단순 타입이면 @RequestParam 도 생략 가능
+    public String requestParamRequired(@RequestParam(required = true) String username, @RequestParam(required = false) int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+    @ResponseBody // view 조회를 무시하고 HTTP message body에 직접 해당 내용 입력
+    @RequestMapping("/request-param-default")
+    // String, int, Integer 등의 단순 타입이면 @RequestParam 도 생략 가능
+    public String requestParamDefault(@RequestParam(defaultValue = "guest") String username, @RequestParam(defaultValue = "-1") int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+    @ResponseBody // view 조회를 무시하고 HTTP message body에 직접 해당 내용 입력
+    @RequestMapping("/request-param-map")
+    // String, int, Integer 등의 단순 타입이면 @RequestParam 도 생략 가능
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
+
+        log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
         return "ok";
     }
 }
