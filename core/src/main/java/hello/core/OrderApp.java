0eeb8626a2a5873b128266e50072629c5523dbdc
JoHyunwoo <jhw3874@gmail.com>
2023-09-20 18:27:52 +0900

    순수 자바에서 스프링으로 전환

--- core/src/main/java/hello/core/OrderApp.java
+++ core/src/main/java/hello/core/OrderApp.java
@@ -7,15 +7,22 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
+//        AppConfig appConfig = new AppConfig();
+//
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
 
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
-//        MemberService memberService = new MemberServiceImpl();
-//        OrderService orderService = new OrderServiceImpl();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+        // applicationContext.getBean 은 빈을 찾는 메서드 이며 빈 이름과 타입을 넘겨줘야 한다.
+        // ApplicationContext 는 스프링 컨테이너 이며, AnnotationConfigApplicationContext 는 스프링 컨테이너를 생성하는 역할을 한다.
+
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP); // 1L is a long type
         memberService.join(member);
