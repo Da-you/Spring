0cd806361bed5e4ee4a9ab409526b9156ac4177f
JoHyunwoo <jhw3874@gmail.com>
2023-09-23 19:29:23 +0900

    애노테이션 직접 만들기

--- core/src/main/java/hello/core/discount/DiscountPolicy.java
+++ core/src/main/java/hello/core/discount/DiscountPolicy.java
@@ -1,6 +1,7 @@
 package hello.core.discount;
 
 import hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public interface DiscountPolicy {
     int discount(Member member, int price);
