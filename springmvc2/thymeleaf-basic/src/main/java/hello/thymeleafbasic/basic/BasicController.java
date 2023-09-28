b0816f36de6dae21f8d59800921a2ae3164838f7
JoHyunwoo <jhw3874@gmail.com>
2023-09-28 18:05:40 +0900

    기본 객체, 유틸리티 객체와 날짜, URL링크, 리터럴

--- springmvc2/thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/BasicController.java
+++ springmvc2/thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/BasicController.java
@@ -1,11 +1,17 @@
 package hello.thymeleafbasic.basic;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -42,6 +48,48 @@ public class BasicController {
         model.addAttribute("users", list);
         model.addAttribute("userMap", map);
         return "basic/variable";
+    }
+
+    // 스프링부트 3.0부터는 "#request, #response, #session, #servletContext"를 지원하지 않는다.
+    @GetMapping("/basic-objects")
+    // 3.0버전 미만일경우
+//    public String basicObjects(HttpSession session) {
+//        session.setAttribute("data", "Hello Spring!");
+//        return "basic/basic-objects";
+//    }
+    public String basicObjects(Model model, HttpServletRequest request,
+                               HttpServletResponse response, HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+        model.addAttribute("request", request);
+        model.addAttribute("response", response);
+        model.addAttribute("servletContext", request.getServletContext());
+        return "basic/basic-objects";
+    }
+
+    @GetMapping("/date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
+
+    @GetMapping("/literal")
+    public String literal(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/literal";
+    }
+
+    @GetMapping("/link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hello " + data;
+        }
     }
 
     @Data
