f35e70c4ef9fe1fa95938da60911a432d04b875f
JoHyunwoo <jhw3874@gmail.com>
2023-10-04 22:38:01 +0900

    기능 연습용 - 인증기능 구현

---
+++ 게시판서비스/demo/src/test/java/com/example/demo/config/TestSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.example.demo.config;
+
+
+import com.example.demo.domain.UserAccount;
+import com.example.demo.repository.UserAccountRepository;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.event.annotation.BeforeTestMethod;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+
+@Import(SecurityConfig.class)
+public class TestSecurityConfig {
+
+    @MockBean private UserAccountRepository userAccountRepository;
+
+    @BeforeTestMethod
+    public void securitySetUp() {
+        given(userAccountRepository.findById(anyString())).willReturn(Optional.of(UserAccount.of(
+                "unoTest",
+                "pw",
+                "uno-test@email.com",
+                "uno-test",
+                "test memo"
+        )));
+    }
+
+}
