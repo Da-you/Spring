e1f5cff392025fca380d2659ccf350434f211c85
JoHyunwoo <jhw3874@gmail.com>
2023-10-20 14:49:06 +0900

    OAuth2 로그인 기능 진행 완료

--- 게시판서비스/demo/src/test/java/com/example/demo/config/TestSecurityConfig.java
+++ 게시판서비스/demo/src/test/java/com/example/demo/config/TestSecurityConfig.java
@@ -2,7 +2,9 @@ package com.example.demo.config;
 
 
 import com.example.demo.domain.UserAccount;
+import com.example.demo.dto.UserAccountDto;
 import com.example.demo.repository.UserAccountRepository;
+import com.example.demo.service.UserAccountService;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.event.annotation.BeforeTestMethod;
@@ -16,16 +18,35 @@ import static org.mockito.BDDMockito.given;
 public class TestSecurityConfig {
 
     @MockBean private UserAccountRepository userAccountRepository;
+    @MockBean private UserAccountService userAccountService;
 
+//    @BeforeTestMethod
+//    public void securitySetUp() {
+//        given(userAccountRepository.findById(anyString())).willReturn(Optional.of(UserAccount.of(
+//                "unoTest",
+//                "pw",
+//                "uno-test@email.com",
+//                "uno-test",
+//                "test memo"
+//        )));
+//    }
     @BeforeTestMethod
     public void securitySetUp() {
-        given(userAccountRepository.findById(anyString())).willReturn(Optional.of(UserAccount.of(
+        given(userAccountService.searchUser(anyString()))
+                .willReturn(Optional.of(createUserAccountDto()));
+        given(userAccountService.saveUser(anyString(), anyString(), anyString(), anyString(), anyString()))
+                .willReturn(createUserAccountDto());
+    }
+
+
+    private UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(
                 "unoTest",
                 "pw",
                 "uno-test@email.com",
                 "uno-test",
                 "test memo"
-        )));
+        );
     }
 
 }
