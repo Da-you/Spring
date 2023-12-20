241a607c10e018eb8cd42993d3dd431bd2a38cb4
JoHyunwoo <jhw3874@gmail.com>
2023-12-20 14:11:35 +0900

    JPA

---
+++ itemservice-db/src/main/java/hello/itemservice/repository/jpa/JpaItemRepository.java
@@ -0,0 +1,78 @@
+package hello.itemservice.repository.jpa;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import java.util.List;
+import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+@Slf4j
+@Repository
+@Transactional
+public class JpaItemRepository implements ItemRepository {
+
+  private final EntityManager em;
+
+  public JpaItemRepository(EntityManager em) {
+    this.em = em;
+  }
+
+  @Override
+  public Item save(Item item) {
+    em.persist(item);
+    return item;
+  }
+
+  @Override
+  public void update(Long itemId, ItemUpdateDto updateParam) {
+    Item item = em.find(Item.class, itemId);
+    item.setItemName(updateParam.getItemName());
+    item.setPrice(updateParam.getPrice());
+    item.setQuantity(updateParam.getQuantity());
+    // em.persist(item); // persist를 호출하지 않아도 된다. 해당 매서드가 끝날떄 트랜잭션이 커밋되면서 flush가 자동으로 호출되기 때문이다.
+
+  }
+
+  @Override
+  public Optional<Item> findById(Long id) {
+    Item item = em.find(Item.class, id);
+    return Optional.ofNullable(item);
+  }
+
+  @Override
+  public List<Item> findAll(ItemSearchCond cond) {
+    String jpql = "select i from Item i";
+    Integer maxPrice = cond.getMaxPrice();
+    String itemName = cond.getItemName();
+    if (StringUtils.hasText(itemName) || maxPrice != null) {
+      jpql += " where";
+    }
+    boolean andFlag = false;
+    if (StringUtils.hasText(itemName)) {
+      jpql += " i.itemName like concat('%',:itemName,'%')";
+      andFlag = true;
+    }
+    if (maxPrice != null) {
+      if (andFlag) {
+        jpql += " and";
+      }
+      jpql += " i.price <= :maxPrice";
+    }
+    log.info("jpql={}", jpql);
+    TypedQuery<Item> query = em.createQuery(jpql, Item.class);
+    if (StringUtils.hasText(itemName)) {
+      query.setParameter("itemName", itemName);
+    }
+    if (maxPrice != null) {
+      query.setParameter("maxPrice", maxPrice);
+    }
+    return query.getResultList();
+  }
+}
