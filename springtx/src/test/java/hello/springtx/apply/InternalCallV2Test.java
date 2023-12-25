8bfb1596199d70e7aefd0888bca4b78bca652305
JoHyunwoo <jhw3874@gmail.com>
2023-12-26 00:02:20 +0900

    프록시 내부 호출시 트랜잭용 적용안됨 해결방법 > 트랜잭션이 적용된 메서드를 내부호출이 아닌 별도의 클래스로 분리하여 외부호출한다.
    * public 메서드만 트랜잭션이 적용된다.(스프링부트 3.0부터는 protected,default에도 적용가능)
    public이 아닌곳에  `@Transactional` 이 붙어 있으면 예외가 발생하지는 않고, 트랜잭션 적용만 무시된다.

--- springtx/src/test/java/hello/springtx/apply/InternalCallV2Test.java
+++ springtx/src/test/java/hello/springtx/apply/InternalCallV2Test.java
@@ -1,5 +1,6 @@
 package hello.springtx.apply;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +12,7 @@ import org.springframework.transaction.support.TransactionSynchronizationManager
 
 @Slf4j
 @SpringBootTest
-class InternalCallV1Test {
+class InternalCallV2Test {
 
   @Autowired
   CallService callService;
@@ -20,30 +21,55 @@ class InternalCallV1Test {
   void printProxy() {
     log.info("callService class={}", callService.getClass());
   }
+
+
+
   @Test
-  void internalCall() {
-    callService.internal();
-  }
-  @Test
-  void externalCall() {
+  void externalCallV2() {
     callService.external();
   }
+
   @TestConfiguration
   static class InternalCallV1Config {
+
     @Bean
     CallService callService() {
-      return new CallService();
+      return new CallService(internalService());
+    }
+
+    @Bean
+    InternalService internalService() {
+      return new InternalService();
     }
   }
+
   @Slf4j
+  @RequiredArgsConstructor
   static class CallService {
+
+    private final InternalService internalService;
 
     public void external() {
       log.info("call external");
       printTxInfo();
-      this.internal();
+      internalService.internal();
     }
 
+    @Transactional
+    public void internal() {
+      log.info("call internal");
+      printTxInfo();
+    }
+
+    private void printTxInfo() {
+      boolean txActive =
+          TransactionSynchronizationManager.isActualTransactionActive();
+      log.info("tx active={}", txActive);
+    }
+  }
+
+  @Slf4j
+  static class InternalService {
     @Transactional
     public void internal() {
       log.info("call internal");
