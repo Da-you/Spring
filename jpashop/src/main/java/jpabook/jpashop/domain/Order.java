6e2d3a167f2a301ce7ba4897bb5ec7a180998b75
JoHyunwoo <jhw3874@gmail.com>
2024-02-12 17:23:44 +0900

    도메인 분석 설계

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,67 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+public class Order {
+
+  @Id
+  @GeneratedValue
+  @Column(name = "oreder_id")
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id")
+  private Member member;
+
+  @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+  private List<OrderItem> orderItems = new ArrayList<>();
+
+  @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+  @JoinColumn(name = "delivery_id")
+  // OneToOne의 경우 어디에 fk를 두어도 무방하나 주로 엑세스를 많이 하는곳에 두자. 주문에서 배송을 찾지 배송에서 주문을 찾지는 않는다
+  private Delivery delivery;
+
+  private LocalDateTime orderDate;
+
+  @Enumerated(EnumType.STRING)
+  private OrderStatus status;
+
+  //==연관관계 메서드==//
+  public void setMember(Member member) {
+    this.member = member;
+    member.getOrders().add(this);
+  }
+
+  public void addOrderItem(OrderItem orderItem) {
+    orderItems.add(orderItem);
+    orderItem.setOrder(this);
+  }
+
+  public void setDelivery(Delivery delivery){
+    this.delivery = delivery;
+    delivery.setOrder(this);
+  }
+
+
+}
